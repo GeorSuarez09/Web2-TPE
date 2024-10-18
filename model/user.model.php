5ab428a827a6be82b27ab4880fe6d3b5cd52ef90
Georgina suarez <suarezgeoor@gmail.com>
2024-10-18 17:03:38 -0300

    Agrego user.model.php para el login

---
+++ model/user.model.php
@@ -0,0 +1,17 @@
+<?php
+class usermodel{
+    private $db;
+    
+    public function __construct()
+    {
+        $this -> db = new PDO('mysql:host=localhost;dbname=viaje_at;charset=utf8', 'root', '');
+    }
+
+    public function getUserByGmail($gmail){
+        $query = $this -> db->prepare("SELECT * FROM usuario WHERE gmail = ?");
+        $query ->execute([$gmail]);
+
+        $user = $query->fetch(PDO::FETCH_OBJ);
+        return $user;
+    }
+}
